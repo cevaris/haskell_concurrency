4dd73ab640a03c8f755cfba89605df911a6872b0
Cevaris <cevaris@gmail.com>
2014-07-02 22:45:11 -0600

    cleaned up data imports

--- src/sudoku/sudoku1.hs
+++ src/sudoku/sudoku1.hs
@@ -3,13 +3,14 @@ import Sudoku
 import Control.Exception
 import System.Environment
 import Data.Maybe
-
+import Paths_hcc
 
 -- <<main
 main :: IO ()
 main = do
   [f] <- getArgs                           -- <1>
-  file <- readFile f                       -- <2>
+  filepath <- getDataFileName f
+  file <- readFile filepath                -- <2>
 
   let puzzles   = lines file               -- <3>
       solutions = map solve puzzles        -- <4>
