a26d405813afb165d4eb772460c3ab808932318c
Adam Cardenas <cevaris@gmail.com>
2014-07-14 15:29:02 -0600

    got working ex 2-1c

--- src/haskell/ex2-1.hs
+++ src/haskell/ex2-1.hs
@@ -9,21 +9,6 @@ exa = ('a':'b':'c':[]) : (('d':'e':[]) : [])
 
 
 -- Write an expression that checks whether a list is empty, [], or its first element is empty, like [[],['a','b']].
---exb :: (Num a) => [a] -> Bool
---exb [] = False
---exb [[], _] = False
---exb (x:xs) = True
-
---exbb :: [[Integer]] -> Bool
---exbb (x:xs) = exba x
-
---exba :: (Num a) => [a] -> Bool
---exba (x:xs) = if null xs
---        then True 
---        else if length (head  x) == 0 
---            then True 
---            else False
-
 exba :: (Num a) => [a] -> Bool
 exba x = if null x
         then True 
@@ -32,28 +17,23 @@ exba x = if null x
 exbb :: (Num a) => [[a]] -> Bool
 exbb (x:xs) = exba x
 
+-- Write an expression that checks whether a list has only one element. It should return True for ['a'] and False for [] or ['a','b'].
+exca :: (Num a) => [a] -> Bool
+exca x = if length x == 1
+        then True 
+        else False
 
---exbb :: [[Integer]] -> Bool
---exbb (null : xs) = False
---exbb (x:xs) = True
+excb :: (Num a) => [[a]] -> Bool
+excb (x:xs) = exba x
 
---sum' :: (Num a) => [a] -> a  
---sum' [] = 0  
---sum' (x:xs) = x + sum' xs  
 
 main = do
-    --print sum' [3, 2, 4]
-    --print exa
-    
-    print (exba [])
-    print (exba [3])
-    print (exbb [[]])
-    print (exbb [[3,4]])
-    --print (exb [3])
-    --print (exbb [[], 3])
-    --print (exb [3, []])
-
-    --print if length [['a'],['a','b']] == 0 then True else if length (head [['a'],['a','b']]) == 0 then True else False
+    print (exca [])
+    print (exca [1])
+    print (exca [1,3])
+    print "-----"
+    print (excb [[]])
+    print (excb [[2],[3]])
 
 
 
