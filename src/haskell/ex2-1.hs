2f63ae63028701608e51484ad753ce3ca52034cd
Adam Cardenas <cevaris@gmail.com>
2014-07-14 15:22:31 -0600

    got working example for ex 2-1b

--- src/haskell/ex2-1.hs
+++ src/haskell/ex2-1.hs
@@ -10,9 +10,28 @@ exa = ('a':'b':'c':[]) : (('d':'e':[]) : [])
 
 -- Write an expression that checks whether a list is empty, [], or its first element is empty, like [[],['a','b']].
 --exb :: (Num a) => [a] -> Bool
-exb [] = False
-exb [[], _] = False
-exb (x:xs) = True
+--exb [] = False
+--exb [[], _] = False
+--exb (x:xs) = True
+
+--exbb :: [[Integer]] -> Bool
+--exbb (x:xs) = exba x
+
+--exba :: (Num a) => [a] -> Bool
+--exba (x:xs) = if null xs
+--        then True 
+--        else if length (head  x) == 0 
+--            then True 
+--            else False
+
+exba :: (Num a) => [a] -> Bool
+exba x = if null x
+        then True 
+        else False
+
+exbb :: (Num a) => [[a]] -> Bool
+exbb (x:xs) = exba x
+
 
 --exbb :: [[Integer]] -> Bool
 --exbb (null : xs) = False
@@ -26,9 +45,15 @@ main = do
     --print sum' [3, 2, 4]
     --print exa
     
-    print (exb [3])
+    print (exba [])
+    print (exba [3])
+    print (exbb [[]])
+    print (exbb [[3,4]])
+    --print (exb [3])
     --print (exbb [[], 3])
     --print (exb [3, []])
+
+    --print if length [['a'],['a','b']] == 0 then True else if length (head [['a'],['a','b']]) == 0 then True else False
 
 
 
