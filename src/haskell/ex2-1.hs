fc78699b5fdf9fde8c4777c15863bdb7f2dac4ff
Adam Cardenas <cevaris@gmail.com>
2014-07-13 21:02:33 -0600

    working through exercise 2-1 slowly

--- src/haskell/ex2-1.hs
+++ src/haskell/ex2-1.hs
@@ -3,10 +3,32 @@
 
 --[['a','b','c'],['d','e']]
 
-ex1 :: [[Char]]
-ex1 =  ('a' : 'b' : 'c' : []) : ('d' : 'e' : []) : []
+-- Rewrite the previous list literals using only (:) and the empty list constructor [].
+exa :: [[Char]]
+exa = ('a':'b':'c':[]) : (('d':'e':[]) : [])
+
+
+-- Write an expression that checks whether a list is empty, [], or its first element is empty, like [[],['a','b']].
+--exb :: (Num a) => [a] -> Bool
+exb [] = False
+exb [[], _] = False
+exb (x:xs) = True
+
+--exbb :: [[Integer]] -> Bool
+--exbb (null : xs) = False
+--exbb (x:xs) = True
+
+--sum' :: (Num a) => [a] -> a  
+--sum' [] = 0  
+--sum' (x:xs) = x + sum' xs  
 
 main = do
-    print ex1
+    --print sum' [3, 2, 4]
+    --print exa
+    
+    print (exb [3])
+    --print (exbb [[], 3])
+    --print (exb [3, []])
+
 
 
