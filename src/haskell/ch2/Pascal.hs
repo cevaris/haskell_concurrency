18cfa58c2fe9251ad216c17937f001bd08a4fd6a
Cevaris <cevaris@gmail.com>
2014-07-21 20:18:14 -0600

    created more concise pascal with bar guards

--- src/haskell/ch2/Pascal.hs
+++ src/haskell/ch2/Pascal.hs
@@ -10,11 +10,18 @@ factorial :: Integer -> Integer
 factorial 0 = 1
 factorial x = x * factorial(x-1)
 
+--pascal :: Integer -> Integer -> Integer
+--pascal n k = if k >= 0
+--             then if k == n
+--                  then 1
+--                  else if n > k 
+--                       then (factorial n) `div` ((factorial k) * factorial (n - k))
+--                       else error "Invalid argument: k > n"
+--             else error "Invalid arguments: k is negative"
+
 pascal :: Integer -> Integer -> Integer
-pascal n k = if k >= 0
-             then if k == n
-                  then 1
-                  else if n > k 
-                       then (factorial n) `div` ((factorial k) * factorial (n - k))
-                       else error "Invalid argument: k > n"
-             else error "Invalid arguments: k is negative"
+pascal n k 
+       | k < 0  = error "Invalid Argument: k is negative."
+       | k > n  = error "Invalid Argument: k > n."
+       | n == k = 1
+       | otherwise = (factorial n) `div` ((factorial k) * factorial (n - k))
